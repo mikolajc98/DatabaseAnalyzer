5ec9d63cbabd68fa759b965f6af97a95ace9d3c1
Mikołaj <mikolajc98@gmail.com>
2021-03-19 23:15:20 +0100

    DatabaseAnalyzer | Added struct that will be used in communication with methods. Prefer structs that has action status and error message than ExceptionDrivenDevelopment.

---
+++ DatabaseAnalyzer/Models/ActionResultStruct.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAnalyzer.Models
+{
+    public struct ActionResult
+    { 
+        ActionResultStatus ActionStatus { get; }
+        string ErrorMessage { get; }
+
+        public ActionResult(ActionResultStatus actionStatus, string errorMessage)
+        {
+            this.ActionStatus = actionStatus;
+            this.ErrorMessage = errorMessage;
+        }
+
+        public bool IsError()
+        {
+            return ActionStatus == ActionResultStatus.Error;
+        }
+
+        public string GetMessage()
+        {
+            return ErrorMessage;
+        }
+    }
+
+    public enum ActionResultStatus
+    {
+        Undefinded = 0,
+        Success = 1,
+        Unknown = 2,
+        Warning = 3,
+        Error = 4
+    }
+}
